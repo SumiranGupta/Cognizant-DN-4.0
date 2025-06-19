230ac1c1abfd45714ee31e885b981179896d865a
Sumiran Gupta <sumirang02@gmail.com>
2025-06-19 19:44:09 +0530

    Add Singleton Pattern .NET solution

--- SingletonPatternExample/obj/Debug/net9.0/SingletonPatternExample.AssemblyInfo.cs
+++ SingletonPatternExample/obj/Debug/net9.0/SingletonPatternExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SingletonPatternExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0c143efac92b0c1a6380c58b54319bd716e37ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("SingletonPatternExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SingletonPatternExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
